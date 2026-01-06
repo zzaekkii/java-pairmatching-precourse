b59354b633649f008e316407a2800264481312d0
zakie <realzzaekkii@gmail.com>
2026-01-06 14:49:04 +0900

    feat: md 파일에서 코스별 크루 목록 읽어오는 기능 구현

--- src/main/java/pairmatching/view/FileInputView.java
+++ src/main/java/pairmatching/view/FileInputView.java
@@ -1,4 +1,29 @@
 package pairmatching.view;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import pairmatching.domain.Crew;
+
 public class FileInputView {
+    public List<Crew> readBackendCrews() throws Exception {
+        List<String> lines = Files.readAllLines(Paths.get("src/main/resources/backend-crew.md"));
+        List<Crew> crews = new ArrayList<>();
+
+        for (String line : lines) {
+            crews.add(new Crew(line));
+        }
+        return crews;
+    }
+
+    public List<Crew> readFrontendCrews() throws Exception {
+        List<String> lines = Files.readAllLines(Paths.get("src/main/resources/frontend-crew.md"));
+        List<Crew> crews = new ArrayList<>();
+
+        for (String line : lines) {
+            crews.add(new Crew(line));
+        }
+        return crews;
+    }
 }
